c50d4e6f70518c013f0247e9e265bf965b96d191
sar37 <158889075+sar37@users.noreply.github.com>
2024-05-01 13:25:09 -0700

    completed

--- task1.py
+++ task1.py
@@ -1,6 +1,22 @@
 def A():
+    print("Hello")
+
+
 
 def B():
+    print("How are you")
 
 
 def C():
+    print("Hi")
+
+L = input("enter in a letter from A - C")
+
+if L == A:
+     A()
+
+elif L == B:
+    B()
+
+elif L ==C:
+    C()
--- task2.py
+++ task2.py
@@ -8,3 +8,21 @@ displays instructions and how to play
 game()
 plays the games
 """
+def title():
+    print("guessing game")
+    print("instructions on how to play: guess a number between 1-5, guess until you get the correct number.")
+    
+def game():
+    n =""
+   
+    while n != 2:
+        n = input("guess a number from 1-5")
+        n = float(n)
+        if n != 2:
+            print("try again")
+        
+        else:
+            print(" Nice, you got it right!")
+
+title()
+game()
